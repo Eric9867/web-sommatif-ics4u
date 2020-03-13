9663f3160b8a457b77c09530fbfc337ff5737ed0
Eric Lefort <eric9867@gmail.com>
2020-03-12 21:18:18 -0400

    So I don't really know how these commit messages work, But I'll give it a shot anyways.
    Still working on the page's structure. Work has started on
    loading zone (zone de chargement) functionality as well
    as a physics animation (pendulum).
    These changes are my own, as well as Xavier Cossettini's (xprogram)
    and Veronica Marrocco's.
    
    Page compatibility with the loading zone needs
    work and css styling is very crude (my bad).
    
    Thanks to all those who have helped me with this.
    Much credit to Xavier Cossettini.

--- pendule.js
+++ pendule.js
@@ -1,59 +1,12 @@
 (function(window){
 "use strict";
 
-class Vec2d {
-  constructor(x, y){
-    this.x = x || 0;
-    this.y = y || 0;
-    this._isVec = true;
-  }
+var omega = 0; // rad/s
+var theta = 1; // rad/s
 
-  produitScalaire(autre){
-    return this.x * autre.x + this.y * autre.y;
-  }
-
-  produitVectoriel(autre){
-    throw new Error('impossible u dumdum');
-  }
-
-  ajouter(autre){
-    if(autre._isVec){
-      this.x += autre.x;
-      this.y += autre.y;
-    } else {
-      this.x += autre;
-      this.y += autre;
-    }
-    return this;
-  }
-
-  multiplier(autre){
-    this.x *= autre;
-    this.y *= autre;
-    return this;
-  }
-
-  diviser(autre){
-    return multiplier(1 / autre);
-  }
-
-  copier(source){
-    this.x = source.x;
-    this.y = source.y;
-    return this;
-  }
-}
-
-var omega = 0;
-var theta = 1;
-
-const vecPos = new Vec2d();
-const vecVel = new Vec2d();
-const vecTmp1 = new Vec2d();
-
-const TERRE_GRAVITE = 9.81;
-const LONGUEUR_PENDULE = 256;
-const ACCELERATION_Y = -10;
+const TERRE_GRAVITE = 9.81; // m/s^2
+const LONGUEUR_PENDULE = 256; // m
+const ACCELERATION_Y = -10; // m/s^2
 
 var dernierMomentReference = performance.now();
 
@@ -65,33 +18,24 @@ function calculerPosition(momentActuel){
 
   // alpha = -g*sin(theta)/r
   // increment omega with alpha and theta with omega
-  let alpha = -TERRE_GRAVITE * Math.sin(theta) / LONGUEUR_PENDULE * 10;
+  let alpha = -TERRE_GRAVITE * Math.sin(theta) / LONGUEUR_PENDULE * 100;
   omega += alpha * deltaT;
   theta += omega * deltaT;
 
   let elemCercle = document.querySelector(".cercle");
+  if(!elemCercle) return;
 
   elemCercle.style.left = `${LONGUEUR_PENDULE * Math.sin(theta)}px`;
-  elemCercle.style.bottom = `${- LONGUEUR_PENDULE * Math.cos(theta) - window.innerHeight / 2}px`;
+  elemCercle.style.bottom = `${-LONGUEUR_PENDULE * Math.cos(theta) - window.innerHeight / 2}px`;
 
-  document.querySelector("line").x1.baseVal.value = (window.innerWidth / 2);
-  document.querySelector("line").y1.baseVal.value = (window.innerHeight / 2);
-  document.querySelector("line").x2.baseVal.value = (window.innerWidth / 2 + LONGUEUR_PENDULE * Math.sin(theta));
-  document.querySelector("line").y2.baseVal.value = (window.innerHeight / 2 + LONGUEUR_PENDULE * Math.cos(theta) - document.querySelector(".cercle").clientHeight / 2);
-  document.querySelector("circle").cx.baseVal.value = (window.innerWidth / 2)
-  document.querySelector("circle").cy.baseVal.value = (window.innerHeight / 2)
-
-/*
-  elemCercle.style.left = `${vecPos.x}px`;
-  elemCercle.style.bottom = `${vecPos.y - 320}px`;
-
-  vecTmp1.copier(vecVel);
-	vecPos.ajouter(vecTmp1.multiplier(deltaT));
-*/
+  let articleElem = document.querySelector("article");
+  document.querySelector("line").x1.baseVal.value = articleElem.clientWidth / 2;
+  document.querySelector("line").y1.baseVal.value = articleElem.clientHeight / 2;
+  document.querySelector("line").x2.baseVal.value = articleElem.clientWidth / 2 + LONGUEUR_PENDULE * Math.sin(theta);
+  document.querySelector("line").y2.baseVal.value = articleElem.clientHeight / 2 + LONGUEUR_PENDULE * Math.cos(theta) - elemCercle.clientHeight / 2;
+  document.querySelector("circle").cx.baseVal.value = articleElem.clientWidth / 2;
+  document.querySelector("circle").cy.baseVal.value = articleElem.clientHeight / 2;
 }
-
-window.addEventListener("load", function(){
-});
 
 requestAnimationFrame(calculerPosition);
 })(window);
